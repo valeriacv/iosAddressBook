c5c9a457ce00f841b18b308597a8b7d5e5f3d72d
Luis Cruz <lcruz@akurey.com>
2019-09-16 09:51:35 -0600

    Initialize Address book assignment

---
+++ AddressBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AddressBook
+//
+//  Created by Vale Calderon  on 9/16/19.
+//  Copyright © 2019 valeria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
